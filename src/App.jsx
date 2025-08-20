f18e217d2c257ee675bab6f21f850ed27804ee6a
Tiltushkin <ivlad6543@gmail.com>
2025-08-20 21:53:57 +0500

    Урок 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import './styles/globals.scss';
-import CardList from "./components/CardList/CardList.jsx";
+import MainLayout from './layouts/MainLayout.jsx';
+import List from './components/List/List.jsx';
+import Adder from './components/Adder/Adder.jsx';
+import { useState } from 'react';
 
 function App() {
+    const [listData, setListData] = useState([])
+
+    function pushData(item) {
+        setListData(prev => [...prev, item]);
+    }
+
+    function removeItem(indexToRemove) {
+        setListData(prev => prev.filter((_, i) => i !== indexToRemove));
+    }
+
     return (
-        <>
-            <h2 className="goods">Текущие товары</h2>
-            <div className="box">
-                <CardList />
+        <MainLayout>
+            <div>
+                <Adder onSubmit={pushData} />
             </div>
-        </>
-    );
+
+            {listData.length > 0 ? (
+                <div>
+                <List items={listData} onDelete={removeItem} />
+                </div>
+            ) : (
+                <p>Лист пустой</p>
+            )}
+        </MainLayout>
+  );
 }
 
 export default App;
---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Layout.module.scss'
+
+function Footer() {
+  return (
+    <footer className={s.footer}>
+      <div className={s.footer__content}>
+        Footer
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
