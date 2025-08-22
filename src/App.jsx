878169d468495a799b28bc0c6bc922870611c6a7
Tiltushkin <109098155+Tiltushkin@users.noreply.github.com>
2025-08-22 21:53:22 +0500

    Простая регистрация (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
 import './styles/globals.scss';
 import MainLayout from './layouts/MainLayout.jsx';
-import List from './components/List/List.jsx';
-import Adder from './components/Adder/Adder.jsx';
-import { useState } from 'react';
+import Registration from './components/Registration/Registration.jsx';
+import ExampleUseEffect from './components/hookExamples/ExampleUseEffect/ExampleUseEffect.jsx';
 
 function App() {
-    const [listData, setListData] = useState([])
 
-    function pushData(item) {
-        setListData(prev => [...prev, item]);
-    }
-
-    function removeItem(indexToRemove) {
-        setListData(prev => prev.filter((_, i) => i !== indexToRemove));
+    function testReg(user) {
+        console.log(`REGISTRATION!\nUser name: ${user.name}\nUser password: ${user.password}`)
     }
 
     return (
         <MainLayout>
-            <div>
-                <Adder onSubmit={pushData} />
-            </div>
-
-            {listData.length > 0 ? (
-                <div>
-                <List items={listData} onDelete={removeItem} />
-                </div>
-            ) : (
-                <p>Лист пустой</p>
-            )}
+            {/* <Registration onSubmit={testReg} /> */}
+            <ExampleUseEffect />
         </MainLayout>
   );
 }
